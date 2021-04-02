83122c0debe84d99c67f2cbc9021a2711bb84958
Konstantin Shishkin <k.shishkin@brandquad.ru>
2021-04-02 13:49:30 +0300

    fix

--- rabbitmquitls.go
+++ rabbitmquitls.go
@@ -55,6 +55,7 @@ func (i *rabbitMQ) Publish(queue string, body interface{}, contenttype string) {
 		false,
 		false,
 		amqp.Publishing{
+			ReplyTo: queue,
 			ContentType: contenttype,
 			Body:        byteBody,
 		})
